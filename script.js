7a36b85d64b0af1ceb2b18c060f968547e422539
Tyler Jennings <tylerjennings2@outlook.com>
2023-05-29 12:49:34 -0500

    Implemented button functions and listeners

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter-btn');
-const newQuoteBtn = document.getElementById('new-quote-btn');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    authorText.textContent = quote.author;
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
     quoteText.textContent = quote.text;
 }
 
@@ -19,8 +29,17 @@ async function getQuotes() {
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-        console.log('Whoops, no quote', error);
+        console.log('No quote', error);
     }
 }
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
